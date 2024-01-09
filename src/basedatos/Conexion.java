7edc604f44b05201e750f69bf8fdb22763997b49
CARPONCHO10 <carpioalexander111@gmail.com>
2024-01-08 19:32:14 -0500

    CAMBIOS

--- src/basedatos/Conexion.java
+++ src/basedatos/Conexion.java
@@ -19,9 +19,13 @@ import java.sql.ResultSet;
  */
 public class Conexion {
 
+    private Statement st;
+    private ResultSet rs;
+
+    private Connection con;
     private Connection conexion;
 
-    String cadenaConexion = "jdbc:postgresql://localhost:5433/Proyecto_Final";
+    String cadenaConexion = "jdbc:postgresql://localhost:5433/Proyecto_final";
 
     String usuarioPG = "postgres";
     String contraPG = "12345";
@@ -42,17 +46,49 @@ public class Conexion {
             System.out.println("Exception" + ex.getMessage());
         }
     }
-    
-    
+
     public ResultSet consultaBD(String sql) {
-        
+
         try {
             Statement st = conexion.createStatement();
             return st.executeQuery(sql);
         } catch (SQLException ex) {
-           System.out.println("Exception" + ex.getMessage());
+            System.out.println("Exception" + ex.getMessage());
             return null;
         }
-        
+
+    }
+
+    public ResultSet selectConsulta(String sql) {
+        try {
+            st = con.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            return rs;
+        } catch (SQLException ex) {
+            java.util.logging.Logger.getLogger(Conexion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
+        }
+        return null;
+    }
+
+    public boolean InsertUpdateDeleteAcciones(String sql) {
+        try {
+            st = con.createStatement();
+            boolean rv = st.execute(sql);
+            st.close();
+            return true;
+        } catch (SQLException ex) {
+            java.util.logging.Logger.getLogger(Conexion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            return false;
+        }
+
+    }
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
     }
 }
