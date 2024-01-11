da2e01de51fdd7aaa3d43a7a7756705d83049fc7
CARPONCHO10 <carpioalexander111@gmail.com>
2024-01-11 02:34:14 -0500

    cambios

--- src/Clases/Puesto.java
+++ src/Clases/Puesto.java
@@ -10,24 +10,21 @@ package Clases;
  * @author Lenovo
  */
 public class Puesto {
-    
+
     private String Codigo_puesto;
     private String NombrePuesto;
     private String DescripcionPuesto;
-    private String Departamento;
-    private String tipo_puesto;
-    private String Salario_puesto;
 
-    public Puesto(String Codigo_puesto, String NombrePuesto, String DescripcionPuesto, String Departamento, String tipo_puesto, String Salario_puesto) {
+    private String tipo_puesto;
+
+    public Puesto() {
+    }
+
+    public Puesto(String Codigo_puesto, String NombrePuesto, String DescripcionPuesto, String tipo_puesto) {
         this.Codigo_puesto = Codigo_puesto;
         this.NombrePuesto = NombrePuesto;
         this.DescripcionPuesto = DescripcionPuesto;
-        this.Departamento = Departamento;
         this.tipo_puesto = tipo_puesto;
-        this.Salario_puesto = Salario_puesto;
-    }
-
-    public Puesto() {
     }
 
     public String getCodigo_puesto() {
@@ -54,14 +51,6 @@ public class Puesto {
         this.DescripcionPuesto = DescripcionPuesto;
     }
 
-    public String getDepartamento() {
-        return Departamento;
-    }
-
-    public void setDepartamento(String Departamento) {
-        this.Departamento = Departamento;
-    }
-
     public String getTipo_puesto() {
         return tipo_puesto;
     }
@@ -70,17 +59,4 @@ public class Puesto {
         this.tipo_puesto = tipo_puesto;
     }
 
-    public String getSalario_puesto() {
-        return Salario_puesto;
-    }
-
-    public void setSalario_puesto(String Salario_puesto) {
-        this.Salario_puesto = Salario_puesto;
-    }
-
-    @Override
-    public String toString() {
-        return "Puesto{" + "Codigo_puesto=" + Codigo_puesto + ", NombrePuesto=" + NombrePuesto + ", DescripcionPuesto=" + DescripcionPuesto + ", Departamento=" + Departamento + ", tipo_puesto=" + tipo_puesto + ", Salario_puesto=" + Salario_puesto + '}';
-    }
-   
 }
