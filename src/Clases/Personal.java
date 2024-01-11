9cdae1e9df5f0496f2eacf3c523779023c04445c
FreddyGomezO <freddy.gomez.est@tecazuay.edu.ec>
2024-01-10 22:10:15 -0500

    CRUDS modificaciones

--- src/Clases/Personal.java
+++ src/Clases/Personal.java
@@ -10,12 +10,25 @@ public class Personal extends Persona{
     private Date Fecha_contrato_inicio;
     private Date Fecha_contrato_fin;
     private String cedula_perso;//llave secundaria de persona
+    private String cod_evento;//llave secundaria de persona
 
     public Personal() {
         super();
     }
 
-    public Personal(String codigo_perso, String Tipo_personal, String Departamento_p, String contanto_p, String Calificacion_p, Date Fecha_contrato_inicio, Date Fecha_contrato_fin, String cedula_perso, String cedula, String nombre, String apellido, String telefono, String correo, String direccion, String celular, Date fecchaNaci, String genero) {
+    public Personal(String codigo_perso, String Tipo_personal, String Departamento_p, String contanto_p, String Calificacion_p, Date Fecha_contrato_inicio, Date Fecha_contrato_fin, String cedula_perso, String cod_evento) {
+        this.codigo_perso = codigo_perso;
+        this.Tipo_personal = Tipo_personal;
+        this.Departamento_p = Departamento_p;
+        this.contanto_p = contanto_p;
+        this.Calificacion_p = Calificacion_p;
+        this.Fecha_contrato_inicio = Fecha_contrato_inicio;
+        this.Fecha_contrato_fin = Fecha_contrato_fin;
+        this.cedula_perso = cedula_perso;
+        this.cod_evento = cod_evento;
+    }
+
+    public Personal(String codigo_perso, String Tipo_personal, String Departamento_p, String contanto_p, String Calificacion_p, Date Fecha_contrato_inicio, Date Fecha_contrato_fin, String cedula_perso, String cod_evento, String cedula, String nombre, String apellido, String telefono, String correo, String direccion, String celular, Date fecchaNaci, String genero) {
         super(cedula, nombre, apellido, telefono, correo, direccion, celular, fecchaNaci, genero);
         this.codigo_perso = codigo_perso;
         this.Tipo_personal = Tipo_personal;
@@ -25,6 +38,7 @@ public class Personal extends Persona{
         this.Fecha_contrato_inicio = Fecha_contrato_inicio;
         this.Fecha_contrato_fin = Fecha_contrato_fin;
         this.cedula_perso = cedula_perso;
+        this.cod_evento = cod_evento;
     }
 
     public String getCodigo_perso() {
@@ -67,14 +81,6 @@ public class Personal extends Persona{
         this.Calificacion_p = Calificacion_p;
     }
 
-    public String getCedula_perso() {
-        return cedula_perso;
-    }
-
-    public void setCedula_perso(String cedula_perso) {
-        this.cedula_perso = cedula_perso;
-    }
-
     public Date getFecha_contrato_inicio() {
         return Fecha_contrato_inicio;
     }
@@ -91,6 +97,28 @@ public class Personal extends Persona{
         this.Fecha_contrato_fin = Fecha_contrato_fin;
     }
 
+    public String getCedula_perso() {
+        return cedula_perso;
+    }
+
+    public void setCedula_perso(String cedula_perso) {
+        this.cedula_perso = cedula_perso;
+    }
+
+    public String getCod_evento() {
+        return cod_evento;
+    }
+
+    public void setCod_evento(String cod_evento) {
+        this.cod_evento = cod_evento;
+    }
+
+    @Override
+    public String toString() {
+        return "Personal{" + "codigo_perso=" + codigo_perso + ", Tipo_personal=" + Tipo_personal + ", Departamento_p=" + Departamento_p + ", contanto_p=" + contanto_p + ", Calificacion_p=" + Calificacion_p + ", Fecha_contrato_inicio=" + Fecha_contrato_inicio + ", Fecha_contrato_fin=" + Fecha_contrato_fin + ", cedula_perso=" + cedula_perso + ", cod_evento=" + cod_evento + '}';
+    }
+
+    
     
       
 }
